c227706b91ab8988910c10aad158123ba98f899e
Joan Llopis <jllopisg@google.com>
2013-03-27 18:01:54 +0100

    Completed install and put functions in own file

--- install.sh
+++ install.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
 INSTALL_DIR=`pwd`
-TIMESTAMP=`date +%Y%m%d%H%M%S`
+TIMESTAMP=`date -u +%Y%m%dT%H%M%SZ`
+LOGFILE=$INSTALL_DIR/install-$TIMESTAMP.log
+# This is the list of packages that should be in your system or installed
+# Note that in OSX Homebrew will always be insalled if not present
+PACKAGES=('git' 'vim')
+FILES=`find despertaferro -type f -maxdepth 1 -exec basename {} \;`
+DIRS=`find despertaferro -type d -maxdepth 1 -exec basename {} \;`
+DOTDIRS=()
+DIRS2BACK=()
+DIRISLINK=()
+DOTFILES=()
+FILES2BACK=()
+FILEISLINK=()
 
 # Include files
 if [[ -r "colors.sh" ]]; then source colors.sh; fi
-
+if [[ -r "functions.sh" ]]; then source functions.sh; else log_error "functions.sh not found. Can't continue :("; exit 1; fi
 
 # Start
 # Select OS
@@ -24,7 +36,26 @@ esac
 
 echo -e "${despertaferro} ${information}installation on ${package}$THIS_OS${text_reset}"
 echo ""
-echo -e "${yellow}\tStarted at `date +%Y-%m-%dT%H:%M:%S`${text_reset}"
+log "Installation started at $TIMESTAMP"
 echo ""
 
+# Backup files and dirs
+backup_dotfiles
 
+# Delete files and directories
+clean_old
+
+# Configure OS
+if [[ "$THIS_OS" == "osx" ]]; then configure_osx; fi
+
+# Install packages
+install_or_update
+
+# Update submdules
+vim_install_submodules
+
+# Link files
+link_dotfiles
+
+echo ""
+log_ok "${despertaferro} ${success}is now installed."
