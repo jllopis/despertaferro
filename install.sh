31b2cb49fa20ec4e83796ee8d12d61ea18cca0bd
Joan Llopis <jllopis@acb.es>
2013-04-24 01:34:09 +0200

    Corrected ordering in find, added sudo for root operations and echo teamocil info

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ LOGFILE=$INSTALL_DIR/install-$TIMESTAMP.log
 # This is the list of packages that should be in your system or installed
 # Note that in OSX Homebrew will always be insalled if not present
 PACKAGES=('git' 'ctags' 'vim' 'tmux' 'go')
-FILES=`find despertaferro -type f -maxdepth 1 -exec basename {} \;`
-DIRS=`find despertaferro -type d -maxdepth 1 -exec basename {} \;`
+FILES=`find despertaferro -maxdepth 1 -type f -exec basename {} \;`
+DIRS=`find despertaferro -maxdepth 1 -type d -exec basename {} \;`
 DOTDIRS=()
 DIRS2BACK=()
 DIRISLINK=()
@@ -20,6 +20,8 @@ if [[ -r "colors.sh" ]]; then source colors.sh; fi
 if [[ -r "functions.sh" ]]; then source functions.sh; else log_error "functions.sh not found. Can't continue :("; exit 1; fi
 
 # Start
+sudo -v
+
 # Select OS
 case `uname` in
  'Darwin')
@@ -39,6 +41,8 @@ echo ""
 log "Installation started at $TIMESTAMP"
 echo ""
 
+# TODO. Copy despertaferro to $HOME/.despertaferro
+
 # Backup files and dirs
 backup_dotfiles
 
@@ -56,6 +60,14 @@ vim_install_submodules
 
 # Link files
 link_dotfiles
+
+echo ""
+echo -e "${despertaferro} ${attention}uses the gem ${component}teamocil${attention} to manage"
+echo -e "tmux sessions. Right now, it is not installed by default. If you want session support, "
+echo -e "remember to install the gem:"
+echo ""
+echo -e "             ${bold_white}gem install teamocil${text_reset}"
+echo ""
 
 echo ""
 log_ok "${despertaferro} ${success}is now installed."
