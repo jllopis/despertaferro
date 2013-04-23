faf7feedee8d5e79e04e6ee6418a7d7a393db651
Joan Llopis <jllopis@acb.es>
2013-04-24 01:31:39 +0200

    Check if running OSX or Linux

--- despertaferro/bashrc.d/git.bash
+++ despertaferro/bashrc.d/git.bash
@@ -1,12 +1,33 @@
-# Brew git completion
-if [ -f `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
-  . `brew --prefix`/etc/bash_completion.d/git-completion.bash
-fi
-if [ -f `brew --prefix`/etc/bash_completion.d/git-prompt.sh ]; then
-  . `brew --prefix`/etc/bash_completion.d/git-prompt.sh
-fi
+case `uname` in
+  'Darwin')
+    # Brew git completion
+    if [ -f `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
+      . `brew --prefix`/etc/bash_completion.d/git-completion.bash
+    fi
+    if [ -f `brew --prefix`/etc/bash_completion.d/git-prompt.sh ]; then
+      . `brew --prefix`/etc/bash_completion.d/git-prompt.sh
+    fi
+    
+    # Brew git-flow completion
+    if [ -f `brew --prefix`/etc/bash_completion.d/git-flow-completion.bash ]; then
+      . `brew --prefix`/etc/bash_completion.d/git-flow-completion.bash
+    fi
+  ;;
+  'Linux')
+    # git completion
+    if [ -f /etc/bash_completion.d/git ]; then
+      . /etc/bash_completion.d/git
+    fi
+    if [ -f /etc/bash_completion.d/git-prompt.sh ]; then
+      . /etc/bash_completion.d/git-prompt.sh
+    elif [ -f /usr/share/git-core/contrib/completion/git-prompt.sh ]; then
+      . /usr/share/git-core/contrib/completion/git-prompt.sh
+    fi
+    
+    # git-flow completion
+    if [ -f /etc/bash_completion.d/git-flow-completion.bash ]; then
+      . /etc/bash_completion.d/git-flow-completion.bash
+    fi
+  ;;
+esac
 
-# Brew git-flow completion
-if [ -f `brew --prefix`/etc/bash_completion.d/git-flow-completion.bash ]; then
-  . `brew --prefix`/etc/bash_completion.d/git-flow-completion.bash
-fi
--- despertaferro/bashrc.d/homebrew.bash
+++ despertaferro/bashrc.d/homebrew.bash
@@ -1,6 +1,12 @@
 echo -e "${information}Loading Homebrew bash completion${text_reset}"
 # Brew bash completion
-if [ -f `brew --prefix`/etc/bash_completion.d/brew_bash_completion.sh ]; then
-  . `brew --prefix`/etc/bash_completion.d/brew_bash_completion.sh
-fi
+case `uname` in
+  'Darwin')
+      if [ -f `brew --prefix`/etc/bash_completion.d/brew_bash_completion.sh ]; then
+        . `brew --prefix`/etc/bash_completion.d/brew_bash_completion.sh
+      fi
+     ;;
+ esac
+
+
 
