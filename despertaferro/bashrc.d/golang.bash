532d9bcf58ff7ba90133b578e3f54882d7e48530
Joan Llopis <jllopisg@gmail.com>
2013-04-24 21:33:00 +0200

    commenting out variables and setting gvm instead

--- despertaferro/bashrc.d/golang.bash
+++ despertaferro/bashrc.d/golang.bash
@@ -1,4 +1,5 @@
-export GOPATH="$HOME/devel/go"
-pathmunge /usr/local/go/bin
-pathmunge $GOPATH/bin
+#export GOPATH="$HOME/devel/go"
+#pathmunge /usr/local/go/bin
+#pathmunge $GOPATH/bin
 
+[[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
