32d7ce71fa7ec33392711247f2cf5897c9163409
Joan Llopis <jllopisg@gmail.com>
2016-04-25 18:26:57 +0200

    Moved go to its own module and changed start selection
    A new subdirectory "modules" has been created. It
    will keep all the files to the modules that will be
    installed. By now, go language installation has been
    moved to it.
    
    Now at the very beginning this directory is scanned
    for the known modules and the files are loaded and
    registered in the MODULES var.
    
    When a module name is passed trough the command line,
    if it is known, the function install_${MODULE_NAME} is
    executed. This will be the entry point for the module
    installation.
    
    The name of the file in the "modules" subdir must match
    with the name of the command line option. E.g. to install
    golang, you will call "install.sh golang" and a module
    file must exist in "modules" named "golang.sh".

--- install.sh
+++ install.sh
@@ -3,13 +3,18 @@ set -e
 
 # install.sh
 #       Will install the deault environment in a computer with either OSX or Ubuntu Linux
-USERNAME=$USER
-REQUIRED_GO_VERSION="go1.6"
-REQUIRED_GO_ARCH="amd64"
-REQUIRED_GO_OS="darwin"
 
+# Global vars
 TIMESTAMP=`date -u +%Y%m%dT%H%M%SZ`
 LOGFILE=${PWD}/install-$TIMESTAMP.log
+USERNAME=$USER
+MODULES=()
+
+# Include files
+if [[ -r "envars.sh" ]]; then source envars.sh; fi
+if [[ -r "colors.sh" ]]; then source colors.sh; fi
+if [[ -r "functions.sh" ]]; then source functions.sh; else log_error "functions.sh not found. Can't continue :("; exit 1; fi
+
 # This is the list of packages that should be in your system or installed
 # Note that in OSX Homebrew will always be insalled if not present
 PACKAGES=('git' 'ctags' 'tmux')
@@ -22,80 +27,37 @@ DOTFILES=()
 FILES2BACK=()
 FILEISLINK=()
 
-# 
-
-# Include files
-if [[ -r "envars.sh" ]]; then source envars.sh; fi
-if [[ -r "colors.sh" ]]; then source colors.sh; fi
-if [[ -r "functions.sh" ]]; then source functions.sh; else log_error "functions.sh not found. Can't continue :("; exit 1; fi
-
-function is_go_installed() {
-	GO_INSTALLED=$(which -s go; echo $?)
-	if [ ${GO_INSTALLED} -eq 1 ]; then
-		GO_VERSION=""
-		return
-	fi
-	GO_PATH=`which go`
-	GO_VERSION=`${GO_PATH} version | cut -d ' ' -f 3`
-	GO_PLATFORM=`${GO_PATH} version |cut -d ' ' -f 4 |tr '/' '-'`
-}
-
-function install_golang() {
-	# Check if go is already installed
-	is_go_installed
-	if [[ $GO_INSTALLED -eq 1 ]]; then
-		if [[ -d /usr/local/go ]]; then
-			echo -e "${attention}A directory /usr/local/go exist. If you continue, it will be removed!${reset}"
-			read -r -p "${attention}Continue? (y/n) ${reset}" resp
-			if [[ $resp =~ ^(s|S|y|Y)$ ]]
-			then
-				echo -ne "${notice}Deleting /usr/local/go}..."
-				rm -rf /usr/local/go
-				echo -ne "${notice}/usr/local/go deleted${reset}"
-			else
-				exit
-			fi
-		else
-			echo -e "${notice}Go not installed. Installing ${REQUIRED_GO_VERSION}${reset}"
-		fi
-	else
-		echo -e "${notice}Go ${GO_VERSION}.${GO_PLATFORM} already installed.${reset}"
-		# ask for reinstall (all version will be removed
-		read -r -p "${attention}Remove and reinstall? (y/n) ${reset}" resp
-		if [[ $resp =~ ^(s|S|y|Y)$ ]]
-		then
-			echo -e "${bold_red}Removing installed version...${reset}"
-			if [ -d ${GO_ROOT} ]; then rm -rf ${GOROOT}; fi
-			echo -e "${bold_red}Reinstalling ${GO_VERSION}.${GO_PLATFORM}${reset}"
-		else
-			echo -e "${attention}Aborting!${reset}"
-			exit
-		fi
-	fi
-
-	# install from https://golang.org/dl/ either osx pkg or linux tgz
-	curl -sL https://golang.org/dl/${REQUIRED_GO_VERSION}.${REQUIRED_GO_OS}-${REQUIRED_GO_ARCH}.tar.gz | gunzip | tar -C /usr/local -xf -
-	echo -e "${attention}${REQUIRED_GO_VERSION}.${REQUIRED_GO_OS}-${REQUIRED_GO_ARCH} installed!${reset}"
-}
-
 function main() {
 	# Select OS
 	case `uname` in
 	'Darwin')
-		THIS_OS="osx"
-		REQUIRED_GO_ARCH="amd64"
-		REQUIRED_GO_OS="darwin"
+		THIS_OS="darwin"
 	;;
 	'Linux')
 		THIS_OS="linux"
-		REQUIRED_GO_ARCH="amd64"
-		REQUIRED_GO_OS="linux"
 	;;
 	*)
 		echo "${failure}Unrecognized OS. Aborting${text_reset}"
 		exit 1
 	;;
 	esac
+
+	if [[ ${THIS_OS} == "osx" ]]; then
+		LOCATOR="which -s"
+	else
+		LOCATOR="type -p"
+	fi
+
+	# Load modules
+	for mod in $(ls -A1 modules/*.sh); do
+		if [[ -r ${mod} ]]; then
+			log "Loading module ${mod}"
+			source ${mod}
+			mod=$(basename ${mod})
+			mod=${mod%.*}
+			MODULES+=(mod)
+		fi
+	done
 	
 	local cmd=$1
 
@@ -103,28 +65,20 @@ function main() {
 		cmd="new"
 	fi
 
-	echo -e "${despertaferro} ${information}Installation Variables:${text_reset}"
-	echo -e "${information}OS:\t${package}$THIS_OS${text_reset}"
-	echo -e "${information}USERNAME:\t${package}$USERNAME${text_reset}"
-	echo -e "${information}HOME DIR:\t${package}$HOME${text_reset}"
-	echo -e "${information}INSTALL DIR:\t${package}$INSTALL_DIR${text_reset}"
-	echo ""
 	log "Installation started at $TIMESTAMP"
-	echo ""
+	log "${information}OS: ${package}$THIS_OS${text_reset}"
+	log "${information}USERNAME: ${package}$USERNAME${text_reset}"
+	log "${information}HOME DIR: ${package}$HOME${text_reset}"
+	log "${information}INSTALL DIR: ${package}$INSTALL_DIR${text_reset}"
 	
-	case $cmd in
-	"golang")
-		log "Installing Go"
-		install_golang
-		;;
-	"new")
-		log "Setting up a new computer"
-		;;
-	*)
+	if [[ -r "modules/${cmd}.sh" ]]; then
+		source modules/${cmd}.sh;
+		install_${cmd} "${@:2}"
+	else
 		log_err "command \"$cmd\" not recognized"
+		show_help
 		exit 1
-		;;
-	esac
+	fi
 
 	# TODO. Copy despertaferro to $HOME/.despertaferro
 	
@@ -150,6 +104,11 @@ function main() {
 	log_ok "${despertaferro} ${success}is now installed."
 }
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Start
-#sudo -v
 main "$@"
