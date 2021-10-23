10b454a8e8df52ba02b26456a0d07c48002e2e7b
Joan Llopis <jllopisg@gmail.com>
2021-10-23 19:13:36 +0200

    added bufferline config setup

--- roles/nvim/files/lua/plugin-bufferline.lua
+++ roles/nvim/files/lua/plugin-bufferline.lua
@@ -1,1 +1,8 @@
-require('bufferline').setup {}
+require('bufferline').setup {
+    options = {
+        numbers = function(opts)
+            return string.format('%s)', opts.id)
+        end,
+        separator_style = "slant",
+    }
+}
