3cad0ea8d1c4a40826379b1bb604a677bd2a7e34
Joan Llopis <jllopisg@gmail.com>
2026-05-21 20:58:38 +0200

    Add Nord colorscheme as default
    - Configure vim.pack.add() to auto-download nord.nvim plugin
    - Create lua/plugins/colorscheme.lua with Nord configuration
    - Nord is now the default colorscheme for neovim

---
+++ dotfiles/default/config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+-- Nord colorscheme configuration
+
+-- Nord settings (before loading)
+vim.g.nord_contrast = true
+vim.g.nord_borders = true
+vim.g.nord_cursorline_transparent = false
+
+-- Load colorscheme
+vim.cmd.colorscheme("nord")
--- dotfiles/default/config/nvim/lua/plugins/init.lua
+++ dotfiles/default/config/nvim/lua/plugins/init.lua
@@ -1,7 +1,12 @@
+-- Automatic plugin download
+vim.pack.add({
+  "https://github.com/shaunsingh/nord.nvim",
+})
+
 -- Plugin configurations loader
 -- Each plugin configuration is in its own file
 
--- require("plugins.colorscheme")
+require("plugins.colorscheme")
 -- require("plugins.telescope")
 -- require("plugins.treesitter")
 -- require("plugins.lsp")
